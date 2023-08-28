f697225bd664792f0d45741fd8f62556d69f4eb4
Harlexq <serhanbakir5@gmail.com>
2023-08-29 01:13:30 +0300

    evalo

---
+++ src/helpers/Process.jsx
@@ -0,0 +1,27 @@
+import User from "../assets/img/icons/user.png";
+import Bar from "../assets/img/icons/bar.png";
+import Shopping from "../assets/img/icons/shopping.png";
+import Briefcase from "../assets/img/icons/briefcase.png";
+
+export const ProcessList = [
+    {
+        img: User,
+        title: "Signup for Service",
+        desc: "Ovitae purus soda duis eu ers auctor augue ultrici cing elit, sed do eius emod tempor incid"
+    },
+    {
+        img: Bar,
+        title: "Research & Development",
+        desc: "Ovitae purus soda duis eu ers auctor augue ultrici cing elit, sed do eius emod tempor incid"
+    },
+    {
+        img: Shopping,
+        title: "Sales & Earning",
+        desc: "Ovitae purus soda duis eu ers auctor augue ultrici cing elit, sed do eius emod tempor incid"
+    },
+    {
+        img: Briefcase,
+        title: "Transfer Funds to Bank",
+        desc: "Ovitae purus soda duis eu ers auctor augue ultrici cing elit, sed do eius emod tempor incid"
+    }
+]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,7 @@ import { faFilePen, faPlay, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { BrandList } from "../helpers/Brands"
 import { DataList } from "../helpers/Data"
 import { FaqList } from "../helpers/Faq"
+import { ProcessList } from "../helpers/Process"
 import { Link } from "react-router-dom"
 
 function Home() {
@@ -178,19 +179,19 @@ function Home() {
                                 </article>
                                 <article className="never-cards-item">
                                     <h2>100K</h2>
-                                    <p>Moudle</p>
+                                    <p>Lines of Code</p>
                                 </article>
                                 <article className="never-cards-item">
                                     <h2>10+</h2>
-                                    <p>Moudle</p>
+                                    <p>Report Modeling</p>
                                 </article>
                                 <article className="never-cards-item">
                                     <h2>50+</h2>
-                                    <p>Moudle</p>
+                                    <p>Color Sceme</p>
                                 </article>
                                 <article className="never-cards-item">
                                     <h2>500K</h2>
-                                    <p>Moudle</p>
+                                    <p>Active user</p>
                                 </article>
                             </div>
                         </div>
@@ -287,6 +288,127 @@ function Home() {
                         <div className="page-text">
                             <h2 className='page-title'>Process</h2>
                             <p className='page-desc'>How It Works</p>
+                            <span className='page-muted'>Bole nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo conididunt ut labore et dolore magna aliqua ut enim ad minim veniam</span>
+                        </div>
+                        <div className="process">
+                            <article className='process-item'>
+                                <div className="process-icon">
+                                    {ProcessList.map((ProcessItem, key) => {
+                                        return (
+                                            <React.Fragment key={key}>
+                                                {key === 0 && <img src={ProcessItem.img} />}
+                                            </React.Fragment>
+                                        );
+                                    })}
+                                </div>
+                                <div className="process-title">
+                                    {ProcessList.map((ProcessItem, key) => {
+                                        return (
+                                            <React.Fragment key={key}>
+                                                {key === 0 && <h2>{ProcessItem.title}</h2>}
+                                            </React.Fragment>
+                                        );
+                                    })}
+                                    {ProcessList.map((ProcessItem, key) => {
+                                        return (
+                                            <React.Fragment key={key}>
+                                                {key === 0 && <p>{ProcessItem.desc}</p>}
+                                            </React.Fragment>
+                                        );
+                                    })}
+                                </div>
+                            </article>
+                            <article className='process-item'>
+                                <div className="process-icon">
+                                    {ProcessList.map((ProcessItem, key) => {
+                                        return (
+                                            <React.Fragment key={key}>
+                                                {key === 1 && <img src={ProcessItem.img} />}
+                                            </React.Fragment>
+                                        );
+                                    })}
+                                </div>
+                                <div className="process-title">
+                                    {ProcessList.map((ProcessItem, key) => {
+                                        return (
+                                            <React.Fragment key={key}>
+                                                {key === 1 && <h2>{ProcessItem.title}</h2>}
+                                            </React.Fragment>
+                                        );
+                                    })}
+                                    {ProcessList.map((ProcessItem, key) => {
+                                        return (
+                                            <React.Fragment key={key}>
+                                                {key === 1 && <p>{ProcessItem.desc}</p>}
+                                            </React.Fragment>
+                                        );
+                                    })}
+                                </div>
+                            </article>
+                            <article className='process-item'>
+                                <div className="process-icon">
+                                    {ProcessList.map((ProcessItem, key) => {
+                                        return (
+                                            <React.Fragment key={key}>
+                                                {key === 2 && <img src={ProcessItem.img} />}
+                                            </React.Fragment>
+                                        );
+                                    })}
+                                </div>
+                                <div className="process-title">
+                                    {ProcessList.map((ProcessItem, key) => {
+                                        return (
+                                            <React.Fragment key={key}>
+                                                {key === 2 && <h2>{ProcessItem.title}</h2>}
+                                            </React.Fragment>
+                                        );
+                                    })}
+                                    {ProcessList.map((ProcessItem, key) => {
+                                        return (
+                                            <React.Fragment key={key}>
+                                                {key === 2 && <p>{ProcessItem.desc}</p>}
+                                            </React.Fragment>
+                                        );
+                                    })}
+                                </div>
+                            </article>
+                            <article className='process-item'>
+                                <div className="process-icon">
+                                    {ProcessList.map((ProcessItem, key) => {
+                                        return (
+                                            <React.Fragment key={key}>
+                                                {key === 3 && <img src={ProcessItem.img} />}
+                                            </React.Fragment>
+                                        );
+                                    })}
+                                </div>
+                                <div className="process-title">
+                                    {ProcessList.map((ProcessItem, key) => {
+                                        return (
+                                            <React.Fragment key={key}>
+                                                {key === 3 && <h2>{ProcessItem.title}</h2>}
+                                            </React.Fragment>
+                                        );
+                                    })}
+                                    {ProcessList.map((ProcessItem, key) => {
+                                        return (
+                                            <React.Fragment key={key}>
+                                                {key === 3 && <p>{ProcessItem.desc}</p>}
+                                            </React.Fragment>
+                                        );
+                                    })}
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='page never bg-light'>
+                <div className="container">
+                    <div className="page-inner never-inner">
+                        <div className="page-text">
+                            <h2 className='page-title'>Testimonial</h2>
+                            <p className='page-desc'>What People Say</p>
                             <span className='page-muted'>Bole nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo conididunt ut labore et dolore magna aliqua ut enim ad minim veniam</span>
                         </div>
                     </div>
